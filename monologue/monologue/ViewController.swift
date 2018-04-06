dd465ef2fcf5ff0e03965962b09ee490f7c72c7d
Johnnie Pittman <jpittman@group6.net>
2018-04-06 00:16:22 -0700

    Initial Xcode Project

---
+++ monologue/monologue/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  monologue
+//
+//  Created by jedi on 4/6/18.
+//  Copyright © 2018 group6. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
