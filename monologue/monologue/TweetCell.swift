2b6456c4b927d737dbcf6a8c95712bd7133747f7
Johnnie Pittman <jpittman@group6.net>
2018-04-19 10:17:26 -0700

    Add a Controller and Cell class file for things in the storyboard.

---
+++ monologue/monologue/TweetCell.swift
@@ -0,0 +1,24 @@
+//
+//  TweetCell.swift
+//  monologue
+//
+//  Created by jedi on 4/19/18.
+//  Copyright © 2018 group6. All rights reserved.
+//
+
+import UIKit
+
+class TweetCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
