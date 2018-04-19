133a6f53ec075287d8faf10410f7546dcb3d81c4
Johnnie Pittman <jpittman@group6.net>
2018-04-19 10:53:32 -0700

    Couple of new class files for the tweet drafts list controller.

---
+++ monologue/monologue/MOTweetStatusCell.swift
@@ -0,0 +1,24 @@
+//
+//  MOTweetStatusCell.swift
+//  monologue
+//
+//  Created by jedi on 4/19/18.
+//  Copyright © 2018 group6. All rights reserved.
+//
+
+import UIKit
+
+class MOTweetStatusCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
